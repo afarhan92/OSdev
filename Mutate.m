add301d31c8cd96d6422a484252d8c63b54c905b
afarhan92 <104193736+afarhan92@users.noreply.github.com>
2022-04-23 09:51:53 +0500

    Add files via upload

---
+++ Mutate.m
@@ -0,0 +1,35 @@
+%
+% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Code: YPEA101
+% Project Title: Implementation of Real-Coded Genetic Algorithm in MATLAB
+% Publisher: Yarpiz (www.yarpiz.com)
+% 
+% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
+% 
+% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
+%
+
+function y=Mutate(x,mu,VarMin,VarMax)
+
+    nVar=numel(x);
+    
+    nmu=ceil(mu*nVar);
+    
+    j=randsample(nVar,nmu);
+    [r,c]=size(x);
+    [rj,cj]=size(j);
+    if (r==1 & cj==1) || (c==1 & rj==1)
+        j=j';
+    end
+    
+    sigma=0.1*(VarMax-VarMin);
+    
+    y=x;
+    y(j)=x(j)+sigma*randn(size(j));
+    
+    y=max(y,VarMin);
+    y=min(y,VarMax);
+
+end
